d450c9707d36e915760b6f7e092b5959e397634f
David <xudavidb@gmail.com>
2015-08-02 18:36:49 -0700

    add authentication

--- routes/accounts.js
+++ routes/accounts.js
@@ -14,9 +14,28 @@ router.post('/register', function(req, res, next) {
   });
 });
 
-/* login a user . */
+/* login a user. Hash it and check it against the hashed password. */
 router.post('/login', function(req, res, next) {
-  res.render('hi');
+  console.log(req.body.username)
+  User.findOne({username: req.body.username}).exec(function(err, object){
+    console.log(object, err)
+    console.log('ho', (err !== null && typeof err !== 'undefined'))
+    console.log('ho2', object === null, typeof object === 'undefined')
+    if((err !== null && typeof err !== 'undefined') || typeof object === 'undefined' || object === null) {
+      console.log('there was an error finding a user')
+      res.send(400, 'There was an error logging in')
+      return
+    }
+    if(object.authenticate(req.body.password)) {
+      console.log('woot')
+      res.send(200, 'yay')
+    }
+    else {
+      console.log('incorrect password')
+      res.send(401, 'Incorrect password')
+    }
+
+  });
 });
 
 /* list  */
